45ef353762a6480f6fabaa6bfa2b6788a61c398e
Eric McCormick <edm00se@gmail.com>
2015-03-05 20:10:55 -0800

    many more updates

--- NSF/Code/Java/com/westeros/model/HouseModel.java
+++ NSF/Code/Java/com/westeros/model/HouseModel.java
@@ -4,21 +4,19 @@ import org.openntf.xsp.model.AbstractSmartDocumentModel;
 
 /**
  * 
- * This is the primary House Model, for how to structure a
- * House, be it great or small, in the Seven Kingdoms of
- * Westeros... or at least this application.
+ * This is the primary House Model, for how to structure a House, be it great or
+ * small, in the Seven Kingdoms of Westeros... or at least this application.
  * 
- * intent: provide wagering elements for sellswords
- * to review hiring houses and hire on contract to them,
- * at the advertised rate
+ * intent: provide wagering elements for sell-swords to review hiring houses and
+ * hire on contract to them, at the advertised rate
  * 
  * @author Eric McCormik, @edm00se
- *
+ * 
  */
 @SuppressWarnings("unused")
 public class HouseModel extends AbstractSmartDocumentModel {
 	private static final long serialVersionUID = 1L;
-	
+
 	private String name;
 	private String description;
 	private String coatOfArms;
@@ -29,17 +27,17 @@ public class HouseModel extends AbstractSmartDocumentModel {
 	private String title;
 	private String heir;
 	private String overlord;
-	
+
 	@Override
 	protected String getFormName() {
 		return "house";
 	}
-	
+
 	@Override
 	public void load(final String unid) {
 		super.load(unid);
 	}
-	
+
 	@Override
 	protected boolean querySave() {
 		return true;
