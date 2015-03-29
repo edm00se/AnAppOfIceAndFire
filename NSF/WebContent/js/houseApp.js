5f871bf2f4f7d7e927fc1303621efc112b76c018
Eric McCormick <edm00se@gmail.com>
2015-03-29 06:27:52 -0700

    basic SPA form and view established, with ui-router

--- NSF/WebContent/js/houseApp.js
+++ NSF/WebContent/js/houseApp.js
@@ -1,18 +1,53 @@
 (function(){
 	
 	//defines the AngularJS app as a module
-	angular.module('houseApp', [])
+	angular.module('houseApp', ['ui.router'])
+
+	//ui-router config
+	.config(
+		function($stateProvider, $urlRouterProvider){
+
+			$urlRouterProvider.otherwise('/houses');
+
+			$stateProvider
+				.state('houses', {
+					url: '/houses',
+					templateUrl: 'partials/houseList.html',
+					controller: 'HouseListCtrl'
+				})
+				.state('houses.item', {
+					url: '/:item',
+					templateUrl: 'partials/house.html',
+					controller: 'OneHouseCtrl'
+				});
+	})
 	
 	//defines the $HTTP factory, one of the 3 service types
-	.factory('myFactory', [ '$http', function($http) {
+	.factory('houseCollectionFactory', [ '$http', function($http) {
 		return $http( {
 			method : 'GET',
 			url : 'xsp/houses'
 		});
 	} ])
+
+	.factory('houseFactory', [ '$http', function($http){
+		return function(id){
+			return $http( {
+				method : 'GET',
+				url : 'xsp/houses/'+id
+			});
+		}
+	}])
+
+	//navigation controller
+	.controller('NavCtrl', function($scope, $location){
+		$scope.isActive = function(route) {
+	    	return route === $location.path();
+	    }
+	})
 	
 	//provies the controller to the app, which handles the interaction of data (model) with the view (a la MVC)
-	.controller('HouseCtrl', function($scope, $filter, myFactory) {
+	.controller('HouseListCtrl', function($scope, $filter, houseCollectionFactory) {
 		
 		//defines filter/search/etc. vars
 		$scope.pageQty = 5; //detectPhone() ? 10 : 30;
@@ -28,21 +63,37 @@
 		
 		$scope.housesOfWesteros = [];
 		//the factory is returning the promise of the $http, so handle success/error here
-		myFactory.success( function(data, status, headers, config) {
-			$scope.housesOfWesteros = data.dataAr;
-			//$scope.predicate = "JobNum";
-			//$scope.reverse = false;
-		}).error( function(data, status, headers, config) {
-			$scope.housesOfWesteros = null;
-			console.log("data: " + data);
-			console.log("status: " + status);
-			console.log("headers: " + headers);
-			console.log("config: " + JSON.parse(config));
-		})
-		 .then( function(){
-			//angular.element('div.screenMask').css('visibility','hidden');
-		});
+		houseCollectionFactory
+			.success( function(data, status, headers, config) {
+				$scope.housesOfWesteros = data.dataAr;
+				//$scope.predicate = "JobNum";
+				//$scope.reverse = false;
+			}).error( function(data, status, headers, config) {
+				$scope.housesOfWesteros = null;
+				console.log("data: " + data);
+				console.log("status: " + status);
+				console.log("headers: " + headers);
+				console.log("config: " + JSON.parse(config));
+			})
+			 .then( function(){
+				//angular.element('div.screenMask').css('visibility','hidden');
+			});
 		
+	})
+	
+	.controller('OneHouseCtrl', function($scope, $stateParams, houseFactory){
+		$scope.item = $stateParams.item;
+		$scope.myHouse = {};
+		houseFactory($scope.item)
+			.success(function(data, status, headers, config) {
+				$scope.myHouse = data;
+			})
+			.error(function(data, status, headers, config) {
+				console.log("status: "+status);
+				console.log("data: "+data);
+				console.log("headers: "+headers);
+				console.log("config: "+JSON.parse(config));
+			});
 	})
 	
 	// we already use the limitTo filter built-in to AngularJS,
